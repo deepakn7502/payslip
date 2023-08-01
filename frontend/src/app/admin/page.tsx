4d6d4d5f404c5deff94625a34c0d4a143e0baef6
DanielGeorge02 <danielgeorge70120@gmail.com>
2023-08-01 08:04:44 +0530

    admin

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,19 +1,34 @@
-import React from 'react'
-// import Picker from 'react-month-picker'
+"use client"
+import { useState } from 'react';
+import Popup from 'reactjs-popup';
+
 
 
 const page = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [isOpen, setIsOpen] = useState(false)
+
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
-        <div className='h-screen w-screen bg-slate-500'>
-            {/* <Picker /> */}
-            <form className='w-full h-24 bg-white'>
+        <div className='h-screen w-screen bg-slate-500 fixed'>
+            <div className=' bg-white w-full h-1/6 flex justify-between mt-5 items-center'>
+                <form className=' h-24 bg-red-600 w-1/2 px-12 flex justify-evenly items-center'>
+                    <input type="text" className='h-12 w-100 ' />
+                    <input type="text" className='h-12 w-100' />
+                    <button className='h-12 w-40 bg-yellow-300'>Button</button>
+                </form>
+                <div className='bg-red-300 h-24 flex justify-center items-center px-9'><Popup trigger={<button> Trigger</button>} position="right center">
+                    <div>Popup content here !!</div>
+                </Popup></div>
+            </div>
+            <div className='h-full bg-green-500'>
 
-                <input type="text" />
-                <input type="text" />
-                <button></button>
+            </div>
 
-            </form>
-        </div>
+        </div >
     )
 }
 
