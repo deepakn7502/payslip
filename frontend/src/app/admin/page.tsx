a36c9168dbec892bfdcb53c446a7c08d15864d96
deepakn7502 <dostmeradeepak@gmail.com>
2023-08-01 08:07:43 +0530

    a

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
-// import Picker from 'react-month-picker'
-
 
 const page = () => {
     return (
         <div className='h-screen w-screen bg-slate-500'>
-            {/* <Picker /> */}
             <form className='w-full h-24 bg-white'>
-
                 <input type="text" />
                 <input type="text" />
                 <button></button>
