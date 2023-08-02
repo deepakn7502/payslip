65a17b5f027dab558cd5b4b11ffb21043ee2de40
deepakn7502 <dostmeradeepak@gmail.com>
2023-08-02 13:26:40 +0530

    admin-page, login-page

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -12,7 +12,7 @@ import { FileUploader } from "react-drag-drop-files";
 import Box from "@mui/material/Box";
 // import XLSX from "xlsx";
 import * as XLSX from "xlsx";
-
+import dataset from "../images/data.json";
 interface Params {
   params: { name: string };
 }
@@ -38,6 +38,7 @@ const page = ({ params }: Params) => {
 
   const [isOpen, setIsOpen] = useState(false);
   const [open, setOpen] = useState(false);
+  const [show, setShow] = useState(false);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -62,74 +63,140 @@ const page = ({ params }: Params) => {
     files.push(file.name);
     console.log({ files });
   };
+
+  const [data, setData] = useState(dataset);
+
+  const handleSearch = (e: any) => {
+    e.preventDefault();
+    setShow(true);
+  };
   return (
-    <div className="h-screen w-screen bg-slate-500 fixed ">
-      <div className=" bg-white w-full h-1/6 flex justify-between mt-5 items-center">
-        <form className=" h-24 bg-red-600 w-1/2 px-12 flex justify-evenly items-center">
-          <input type="text" className="h-12 w-100 " />
-          <input type="text" className="h-12 w-100" />
-          <button className="h-12 w-40 bg-yellow-300">Button</button>
-        </form>
-        <div className="bg-red-300 h-24 flex justify-center items-center px-9">
-          <button onClick={handleOpen}>Upload</button>
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
+    <div className="h-screen w-screen">
+      <div className="bg-blue-950 w-full h-24 mt-6 grid grid-cols-7 place-items-center gap-4">
+        <form className="w-full grid grid-cols-5 col-span-3 gap-4 pl-4">
+          <input
+            type="text"
+            className="h-12 w-full col-span-2 rounded-md text-black"
+          />
+          <input
+            type="text"
+            className="h-12 w-full col-span-2 rounded-md text-black"
+          />
+          <button
+            className="h-12 w-28 bg-yellow-300 col-span-1 rounded-lg text-black"
+            onClick={handleSearch}
           >
-            <Box sx={style} borderRadius={5}>
-              <div className="grid h-full w-full grid-cols-1  place-items-center ">
-                <h1 className="text-3xl font-bold ">UPLOAD FILE</h1>
-                <div className="w-full flex flex-col items-center justify-evenly h-[200px]">
-                  <h1 className="text-xl">
-                    Drag and Drop files here or browse
-                  </h1>
-                  <FileUploader
-                    handleChange={handleChange}
-                    name="file"
-                    types={fileTypes}
-                  />
-                </div>
-                <div className="h-[200px] w-full bg-orange-300 grid grid-cols-2 ">
-                  <div className="w-full flex flex-col items-center">
-                    <h1>Files Uploaded</h1>
-                    {files?.map(
-                      (
-                        file:
-                          | string
-                          | number
-                          | boolean
-                          | ReactElement<
-                              any,
-                              string | JSXElementConstructor<any>
-                            >
-                          | Iterable<ReactNode>
-                          | ReactPortal
-                          | PromiseLikeOfReactNode
-                          | null
-                          | undefined
-                      ) => {
-                        // eslint-disable-next-line react/jsx-key
-                        return <h1>{file}</h1>;
-                      }
-                    )}
-                  </div>
-                  <div className="w-full flex justify-center">File Status</div>
-                </div>
-                {/* <input type="file" onChange={(e) => { if (e.target.files != null) { setFile(e.target.files[0]) } }} accept=".xlxs,.xls" /> */}
-                <button
-                  className="w-[100px] h-[40px] bg-orange-300 rounded-md"
-                  onClick={handleFileRead}
-                >
-                  Upload
-                </button>
-              </div>
-            </Box>
-          </Modal>
-        </div>
+            Search
+          </button>
+        </form>
+        <form className="w-full grid grid-cols-5 col-span-3 gap-4">
+          <input
+            type="text"
+            className="h-12 w-full col-span-2 rounded-md text-black"
+          />
+          <input
+            type="text"
+            className="h-12 w-full col-span-2 rounded-md text-black"
+          />
+          <button className="h-12 w-28 bg-yellow-300 col-span-1 rounded-lg text-black">
+            Filter
+          </button>
+        </form>
+        <form className="w-full grid grid-cols-1 col-span-1 place-items-center">
+          <button
+            className="h-12 w-28 bg-yellow-300 col-span-1 rounded-lg text-black"
+            onClick={handleOpen}
+          >
+            Upload
+          </button>
+        </form>
       </div>
-      <div className="h-full bg-green-500"></div>
+      <div className="h-full">
+        {show ? (
+          <div>
+            <table className="w-4/5 mx-auto my-4">
+              <thead>
+                <tr className="h-12 bg-blue-950">
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Department</th>
+                  <th>Designation</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data?.map((person) => {
+                  return (
+                    <tr className="h-8 text-black text-center">
+                      <td>{person.id}</td>
+                      <td>{person.first_name}</td>
+                      <td>{person.department}</td>
+                      <td>{person.designation}</td>
+                      <td>{person.email}</td>
+                      <td>{person.phoneno}</td>
+                      <td>{person.status}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} borderRadius={5}>
+          <div className="grid h-full w-full grid-cols-1  place-items-center ">
+            <h1 className="text-3xl font-bold ">UPLOAD FILE</h1>
+            <div className="w-full flex flex-col items-center justify-evenly h-[200px]">
+              <h1 className="text-xl">Drag and Drop files here or browse</h1>
+              <FileUploader
+                handleChange={handleChange}
+                name="file"
+                types={fileTypes}
+              />
+            </div>
+            <div className="h-[200px] w-full bg-orange-300 grid grid-cols-2 ">
+              <div className="w-full flex flex-col items-center">
+                <h1>Files Uploaded</h1>
+                {files?.map(
+                  (
+                    file:
+                      | string
+                      | number
+                      | boolean
+                      | ReactElement<any, string | JSXElementConstructor<any>>
+                      | Iterable<ReactNode>
+                      | ReactPortal
+                      | PromiseLikeOfReactNode
+                      | null
+                      | undefined
+                  ) => {
+                    // eslint-disable-next-line react/jsx-key
+                    return <h1>{file}</h1>;
+                  }
+                )}
+              </div>
+              <div className="w-full flex justify-center">File Status</div>
+            </div>
+            {/* <input type="file" onChange={(e) => { if (e.target.files != null) { setFile(e.target.files[0]) } }} accept=".xlxs,.xls" /> */}
+            <button
+              className="w-[100px] h-[40px] bg-orange-300 rounded-md"
+              onClick={handleFileRead}
+            >
+              Upload
+            </button>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 };
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,23 @@
 export default function Login() {
   return (
     <div>
-      <form className="h-96 bg-white w-96 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-around border-2 rounded-2xl border-blue-500 ">
-        <h1 className="text-blue-500 text-3xl uppercase">login</h1>
+      <form className="h-96 bg-blue-950 w-96 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-around border-2 rounded-2xl border-blue-950 ">
+        <h1 className="text-white text-3xl uppercase">login</h1>
         <input
-          className="h-14 w-10/12 px-4 text-black border-2 border-blue-500 rounded-lg"
+          className="h-14 w-10/12 px-4 text-black border-2 border-blue-950 rounded-lg"
           placeholder="Username"
         />
         <input
-          className="h-14 w-10/12 px-4 text-black border-2 border-blue-500 rounded-lg"
+          className="h-14 w-10/12 px-4 text-black border-2 border-blue-950 rounded-lg"
           placeholder="Password"
         />
         <button
-          className="bg-blue-500 px-4 py-2 rounded text-white"
+          className="bg-yellow-300 px-4 py-2 rounded text-black"
           type="submit"
         >
           Login
         </button>
-        <button className="text-blue-500">Forgot Password?</button>
+        <button className="text-white">Forgot Password?</button>
       </form>
     </div>
   );
