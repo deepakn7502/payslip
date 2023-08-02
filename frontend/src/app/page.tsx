b22c9881daad8b6b5b7344312ee5b8a4ee26f1a1
deepakn7502 <dostmeradeepak@gmail.com>
2023-08-02 11:23:36 +0530

    sac

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -24,3 +24,15 @@ export default function Login() {
 }
 
 // className="bg-[url('/images/vivid-blurred.jpg')]"
+
+// type Repository {
+//   id: number;
+//   name: string;
+//   full_name: string;
+//   };
+//   export default async function Page() {
+//   | const res = await fetch('https://api.github.com/
+//   repos/vercel/next.js');
+//   const data: Repository await res.json();
+//   return <h1>{data.full_name}</h1>;
+//   }
