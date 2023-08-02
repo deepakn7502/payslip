6de45c5efb8c19f02f54188c704937efe4d210bf
DanielGeorge02 <danielgeorge70120@gmail.com>
2023-08-02 12:49:11 +0530

    admin

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,8 +1,18 @@
 "use client"
-import { JSXElementConstructor, PromiseLikeOfReactNode, ReactElement, ReactNode, ReactPortal, useState } from 'react';
+import { useState } from 'react';
+import { Poppins } from 'next/font/google';
 import Modal from '@mui/material/Modal';
 import { FileUploader } from "react-drag-drop-files";
 import Box from '@mui/material/Box';
+import { IoIosCloseCircle } from "react-icons/io";
+
+
+const poppins = Poppins({
+    weight: ["400"],
+    subsets: ["latin"],
+    style: 'italic'
+
+})
 
 
 const page = () => {
@@ -35,6 +45,8 @@ const page = () => {
     const handleChange = (file: any) => {
         setFile(file);
 
+
+
         console.log(file);
     };
     return (
@@ -62,12 +74,12 @@ const page = () => {
                                 <div className="w-full flex flex-col items-center justify-evenly h-[200px]">
                                     <h1 className='text-xl'>Drag and Drop files here or browse</h1>
                                     <FileUploader handleChange={handleChange} label="Upload file" name="file" types={fileTypes} /></div>
-                                <div className='h-1/2 w-full bg-orange-300 grid grid-cols-2 '>
-                                    <div className='w-full flex flex-col items-center'><h1>Files Uploaded</h1>
-                                        <h1>{file?.name}</h1>
-                                    </div>
-                                    <div className='w-full flex flex-col items-center'><h1>File Status</h1>
-                                        {file.name ? <h1>Success</h1> : <h1></h1>}
+                                <div className='h-[100px] w-full bg-[rgb(246,237,212)] grid grid-cols-2 place-items-center border-dashed border-2 border-black rounded-lg'>
+                                    <div className='w-full flex flex-col items-center'><h1 className='font-bold'>Files Uploaded</h1>
+                                        {file.name ? <h1 className={poppins.className}>{file?.name}</h1> : <h1 className={`${poppins.className} `}>No Files Uploaded</h1>}
+                                    </div>     
+                                    <div className='w-full flex flex-col items-center'><h1 className='font-bold'>File Status</h1>
+                                        {file.name ? <h1 className={` text-green-500  ${poppins.className}`}>Success</h1> : <h1>--</h1>}
                                     </div>
 
 
