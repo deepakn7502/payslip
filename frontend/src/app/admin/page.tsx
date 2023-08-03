12c0cd312f095b482b3a4fb886bf9ea4e580596f
DanielGeorge02 <danielgeorge70120@gmail.com>
2023-08-03 08:30:29 +0530

    modal

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -14,7 +14,7 @@ const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
     props,
     ref,
 ) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    return <MuiAlert elevation={10} ref={ref} variant="filled" {...props} />;
 });
 
 const poppins = Poppins({
@@ -92,7 +92,7 @@ const page = () => {
                             <div className="grid h-full w-full grid-cols-1  place-items-center ">
                                 <h1 className='text-3xl font-bold '>UPLOAD FILE</h1>
                                 <div className="w-full flex flex-col items-center justify-evenly h-[200px]">
-                                    <h1 className='text-xl'>Drag and Drop files here or browse</h1>
+                                    <h1 className='text-xl font-bold'>Drag and Drop files here or browse</h1>
                                     <FileUploader handleChange={handleChange} label="Upload file" name="file" types={fileTypes} /></div>
                                 <div className='h-[100px] w-full bg-[rgb(246,237,212)] grid grid-cols-2 place-items-center border-dashed border-2 border-black rounded-lg'>
                                     <div className='w-full flex flex-col items-center'><h1 className='font-bold'>Files Uploaded</h1>
@@ -101,19 +101,21 @@ const page = () => {
                                     <div className='w-full flex flex-col items-center'><h1 className='font-bold'>File Status</h1>
                                         {file.name ?
                                             <div className='flex w-[100px] justify-between items-center'> <h1 className={` text-green-500  ${poppins.className}`}>Success</h1>
-                                                <Tooltip title="Remove uploaded File"><div className='cursor-pointer' onClick={Remove}><IoIosCloseCircle size={20} /><Snackbar open={snack} autoHideDuration={6000} onClose={closeSnack}>
-                                                    <Alert onClose={closeSnack} severity="error" sx={{ width: "100%" }}>
-                                                        File Removed!
-                                                    </Alert>
-                                                </Snackbar></div></Tooltip>
+                                                <Tooltip title="Remove uploaded File"><div className='cursor-pointer' onClick={Remove}><IoIosCloseCircle size={20} /></div></Tooltip>
 
                                             </div>
                                             : <h1>--</h1>}
                                     </div>
                                 </div>
-                                <button className='w-[100px] h-[40px] bg-[rgb(255,193,7)] rounded-md' >Upload</button>
+                                <button className='w-28 h-10 bg-yellow-300 rounded-md' >Upload</button>
                             </div>
+                            <Stack sx={{ width: '100%' }}>  <Snackbar anchorOrigin={{ 'horizontal': 'center', 'vertical': 'top' }} open={snack} autoHideDuration={2000} onClose={closeSnack}>
+                                <Alert onClose={closeSnack} severity="error" sx={{ width: "100%" }}>
+                                    File Removed!
+                                </Alert>
+                            </Snackbar></Stack>
                         </Box>
+
                     </Modal>
                 </div>
             </div>
@@ -121,6 +123,8 @@ const page = () => {
 
 
             </div>
+
+
 
 
         </div >
