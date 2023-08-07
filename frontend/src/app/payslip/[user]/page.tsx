f29e9d5ba4f3f5d81e9a2b59027b07564a0fb01f
deepakn7502 <dostmeradeepak@gmail.com>
2023-08-07 14:03:06 +0530

    hello

--- frontend/src/app/payslip/[user]/page.tsx
+++ frontend/src/app/payslip/[user]/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-import image from "../images/pec-logo.png";
+import image from "../../images/pec-logo.png";
 import { Blinker } from "next/font/google";
 import { Avatar } from "@mui/material";
 
@@ -16,7 +16,7 @@ interface Params {
 export default function PDF({ params }: Params) {
   return (
     <div className="h-screen w-full bg-blue-950">
-      <div className="bg-blue-950 h-[75px] w-full grid grid-cols-11 items-center text-center">
+      <div className="bg-blue-950 h-[75px]  text-white w-full grid grid-cols-11 items-center text-center">
         <Image
           src={image}
           alt="Logo"
@@ -27,14 +27,13 @@ export default function PDF({ params }: Params) {
         />
 
         <h1
-          className={` ${blinker.className} col-start-4 col-end-9 text-[37px] font-[]`}
+          className={` ${blinker.className}   col-start-4 col-end-9 text-[50px] font-[]`}
         >
-          PANIMALAR ENGINEERING COLLEGE
+          PAYCHECK SLIPS
         </h1>
         <div className="col-start-10 col-end-13 flex items-center justify-around ">
           <Avatar src="/broken-image.jpg" />
-          {/* <p className="text-xl">{params.user.replace("%20", " ")}</p> */}
-          <p className="text-xl">P</p>
+          <p className="text-xl">{params.user.replace("%20", " ")}</p>
           <ExitToAppIcon
             fontSize="large"
             className="cursor-pointer"
@@ -42,132 +41,129 @@ export default function PDF({ params }: Params) {
         </div>
       </div>
       <div className="bg-white h-fit w-[950px] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl">
-        <table className="w-11/12 mx-auto my-10">
-          <thead className="w-full text-center">
-            <tr>
-              <td rowSpan={6}>
+        <table className="w-11/12 mx-auto my-10 grid grid-cols-1">
+          <thead className="w-full text-center grid grid-cols-6  place-items-center">
+            <tr className="col-start-1 col-end-2 row-start-1 row-end-6">
+              <td>
                 <Image
-                  src={require("../images/logo.jpg")}
+                  src={require("../../images/logo.jpg")}
                   alt="Logo"
                   width={1000}
                   height={1000}
-                  className="h-24 w-auto"
+                  className="w-11/12 h-auto"
                 />
               </td>
             </tr>
-            <tr>
-              <td colSpan={3} className="font-bold text-2xl">
+            <tr className=" col-start-2 col-end-7">
+              <td className="font-bold text-3xl">
                 <h1>PANIMALAR ENGINEERING COLLEGE</h1>
               </td>
             </tr>
-            <tr>
-              <td colSpan={3} className="text-xs">
+            <tr className=" col-start-2 col-end-7">
+              <td className="text-xs">
                 <h1>(An Autonomous Institution)</h1>
               </td>
             </tr>
-            <tr>
-              <td colSpan={3} className="text-sm">
+            <tr className=" col-start-2 col-end-7">
+              <td className="text-sm">
                 <h1>Jaisakthi Educational Trust</h1>
               </td>
             </tr>
-            <tr>
-              <td colSpan={3} className="text-xs">
+            <tr className=" col-start-2 col-end-7">
+              <td className="text-xs">
                 <h1>Chennai - 600123</h1>
               </td>
             </tr>
-            <tr>
-              <td colSpan={3} className="text-base font-bold">
+            <tr className=" col-start-2 col-end-7">
+              <td className="text-base font-bold">
                 <h1>PaySlip for the Month of August 2023</h1>
               </td>
             </tr>
           </thead>
           <tbody className="border-2 border-black">
-            <tr>
-              <td colSpan={2}>
-                <h1>
-                  Name:
-                  <span>Anandha simha m</span>
-                </h1>
+            <tr className="grid grid-cols-4">
+              <td className="col-span-2">
+                <h1>Name:</h1>
+                <p>Anandha simha m</p>
               </td>
-              <td>
+              <td className="col-span-1">
                 <h1>
                   EMPID:
-                  <span>PECMEC0057</span>
+                  <p>PECMEC0057</p>
                 </h1>
               </td>
-              <td>
+              <td className="col-span-1">
                 <h1>
                   A/C No:
-                  <span>920010002624855</span>
+                  <p>920010002624855</p>
                 </h1>
               </td>
             </tr>
-            <tr>
-              <td colSpan={2}>
+            <tr className="grid grid-cols-4">
+              <td className="col-span-2">
                 <h1>
-                  Department
-                  <span>MECH</span>
+                  Department :<p>MECH</p>
                 </h1>
               </td>
-              <td>
+              <td className="col-span-1">
                 <h1>
                   Bank
-                  <span>Axis bank</span>
+                  <p>Axis bank</p>
                 </h1>
               </td>
-              <td>
+              <td className="col-span-1">
                 <h1>
                   branch
-                  <span>kilpauk</span>
+                  <p>kilpauk</p>
                 </h1>
               </td>
             </tr>
-            <tr>
-              <td colSpan={2}>
+            <tr className="grid grid-cols-4">
+              <td className="col-span-2">
                 <h1>
                   designation
-                  <span>asst.professor(lab)</span>
+                  <p>asst.professor(lab)</p>
                 </h1>
               </td>
-              <td>
+              <td className="col-span-1">
                 <h1>
                   pan
-                  <span>ABCD1234E</span>
+                  <p>ABCD1234E</p>
                 </h1>
               </td>
-              <td>
+              <td className="col-span-1">
                 <h1>
                   uan
-                  <span>101464899043</span>
+                  <p>101464899043</p>
                 </h1>
               </td>
             </tr>
-            <tr>
-              <td colSpan={2}>
+            <tr className="grid grid-cols-2">
+              <td className="col-span-1 text-center">
                 <h1>earnings(₹)</h1>
               </td>
-              <td colSpan={2}>
+              <td className="col-span-1 text-center">
                 <h1>deductions(₹)</h1>
               </td>
             </tr>
-            <tr>
+            <tr className="grid grid-cols-4 ">
               <td>
                 <tr>
                   <h1>
                     basic pay-
-                    <span>8841</span>
+                    <p>8841</p>
                   </h1>
                 </tr>
                 <tr>
                   <h1>
                     da
-                    <span>12024</span>
+                    <p>12024</p>
                   </h1>
                 </tr>
                 <tr>
                   <h1>
                     leave
-                    <span>0.00</span>
+                    <p>0.00</p>
                   </h1>
                 </tr>
               </td>
@@ -175,75 +171,67 @@ export default function PDF({ params }: Params) {
                 <tr>
                   <h1>
                     basic-1-
-                    <span>8841</span>
+                    <p>8841</p>
                   </h1>
                 </tr>
                 <tr>
                   <h1>
                     hra
-                    <span>884</span>
+                    <p>884</p>
                   </h1>
                 </tr>
                 <tr>
                   <h1>
-                    <span>-</span>
+                    <p>-</p>
                   </h1>
                 </tr>
               </td>
               <td>
                 <tr>
                   <h1>
-                    p f<span>2504</span>
+                    p f<p>2504</p>
                   </h1>
                 </tr>
                 <tr>
                   <h1>
-                    p t<span>0</span>
+                    p t<p>0</p>
                   </h1>
                 </tr>
                 <tr>
                   <h1>
-                    i t<span>0</span>
+                    i t<p>0</p>
                   </h1>
                 </tr>
               </td>
               <td>
                 <tr>
-                  <h1>
-                    miscellaneous-
-                    <span>0</span>
-                  </h1>
+                  <h1>miscellaneous-</h1>
+                  <p>0</p>
                 </tr>
                 <tr>
-                  <h1>
-                    esi-
-                    <span></span>
-                  </h1>
+                  <h1>esi-</h1>
+                  <p>0</p>
                 </tr>
                 <tr>
-                  <h1>
-                    deductions -<span></span>
-                  </h1>
+                  <h1>deductions -</h1>
+                  <p>0</p>
                 </tr>
               </td>
             </tr>
-            <tr>
-              <td colSpan={2}>
-                <h1>
-                  total earnings: <span>21749</span>
-                </h1>
+            <tr className="grid grid-cols-2">
+              <td className="col-span-1 text-center">
+                <h1>total earnings:</h1>
+                <p>21749</p>
               </td>
-              <td colSpan={2}>
-                <h1>
-                  total deductions: <span>2504</span>
-                </h1>
+              <td className="col-span-1 text-center">
+                <h1>total deductions:</h1>
+                <p>2504</p>
               </td>
             </tr>
-            <tr>
-              <td colSpan={4}>
-                <h1>
-                  net salary(₹) <span>19245</span>
-                </h1>
+            <tr className="grid grid-cols-1">
+              <td className="text-center">
+                <h1>net salary(₹)</h1>
+                <p>19245</p>
               </td>
             </tr>
           </tbody>
